d3aeb87fccaa8e0215e8b3d98a0d06844b7a46a9
Gabriel <sana.gp@gmail.com>
2022-04-18 23:39:01 -0300

    Dependencies fixing and some DataTables improvement.

--- src/main/java/dev/vfcardoso/poc/api/controllers/UserController.java
+++ src/main/java/dev/vfcardoso/poc/api/controllers/UserController.java
@@ -1,19 +1,26 @@
 package dev.vfcardoso.poc.api.controllers;
 
+import dev.vfcardoso.poc.business.dao.UserDtDao;
 import dev.vfcardoso.poc.business.exceptions.AppException;
 import dev.vfcardoso.poc.business.models.User;
 import dev.vfcardoso.poc.business.repositories.UserRepository;
+import dev.vfcardoso.poc.helper.arch.datatables.DataTables;
+import dev.vfcardoso.poc.helper.arch.datatables.OrderWrapper;
+import dev.vfcardoso.poc.helper.datatables.DtUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Named;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired private UserRepository userRepository;
+
+    @Autowired
+    private UserDtDao userDtDao;
+
 
     @GetMapping
     public Iterable findAll() {
@@ -25,5 +32,27 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(AppException::new);
     }
+
+
+
+    @PostMapping("/list.json")
+    public DataTables<DtUser> listDtJson(Integer draw, DtUser dtUser,
+                                         int start, int length ) {
+        try {
+            Long associacaoId = 0L;
+            DataTables<DtUser> dataTables = userDtDao.listJson(dtUser, associacaoId,start, length, 2, "asc");
+            dataTables.setDraw(draw);
+            //result.use(Results.json()).withoutRoot().from(dataTables).include("data").serialize();
+
+            return dataTables;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return new DataTables<>();
+        }
+    }
+
+
 
 }
--- src/main/java/dev/vfcardoso/poc/controllers/web/UserController.java
+++ src/main/java/dev/vfcardoso/poc/controllers/web/UserController.java
@@ -1,17 +1,38 @@
 package dev.vfcardoso.poc.controllers.web;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import dev.vfcardoso.poc.business.dao.UserDtDao;
+import dev.vfcardoso.poc.helper.arch.datatables.DataTables;
+import dev.vfcardoso.poc.helper.arch.datatables.OrderWrapper;
+import dev.vfcardoso.poc.helper.datatables.DtUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Named;
+import javax.xml.crypto.Data;
+import java.util.List;
+import java.util.Map;
+
 
 @Controller("WebUserController")
 @RequestMapping("/dashboard/users")
 public class UserController {
 
+
+
+
+
+
+
+
     @GetMapping("/list")
-    public String list(Model model) {
-        return "pages/dashboard/user/list";
-    }
+    public String list(Model model) { return "pages/dashboard/user/list"; }
+
+
 
 }
