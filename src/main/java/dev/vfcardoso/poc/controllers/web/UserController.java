a20c7a730fc0216864f9f1d9d4c8c0e9c096584e
Gabriel <sana.gp@gmail.com>
2022-05-04 23:58:03 -0300

    Create user form - persisting object

--- src/main/java/dev/vfcardoso/poc/controllers/web/UserController.java
+++ src/main/java/dev/vfcardoso/poc/controllers/web/UserController.java
@@ -3,6 +3,8 @@ package dev.vfcardoso.poc.controllers.web;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import dev.vfcardoso.poc.business.dao.UserDtDao;
 import dev.vfcardoso.poc.business.models.User;
+import dev.vfcardoso.poc.business.repositories.UserRepository;
+import dev.vfcardoso.poc.business.valueobjects.SecurityRole;
 import dev.vfcardoso.poc.helper.arch.datatables.DataTables;
 import dev.vfcardoso.poc.helper.arch.datatables.OrderWrapper;
 import dev.vfcardoso.poc.helper.datatables.DtUser;
@@ -24,10 +26,17 @@ import java.util.Map;
 @RequestMapping("/dashboard/users")
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("securityRoles", SecurityRole.values());
+        //SecurityRole role  =  SecurityRole.USER;
+        //role.ordinal();
+        //role.name();
+
         return "pages/dashboard/user/create";
     }
 
@@ -37,8 +46,16 @@ public class UserController {
 
     @PostMapping("/add")
     public String add(@ModelAttribute User user, Model model) {
-        model.addAttribute("user", user);
-        return "pages/dashboard/user/list";
+        try{
+            userRepository.save(user);
+            model.addAttribute("message", "Usuário Adicionado.");
+            return "pages/dashboard/user/list";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", e.getMessage());
+            model.addAttribute("securityRoles", SecurityRole.values());
+            return "pages/dashboard/user/create";
+        }
+
     }
 
 
